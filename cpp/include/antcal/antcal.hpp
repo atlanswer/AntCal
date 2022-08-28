57f024741dede2c1f90f7796216cf92a4a2d0cff
Atlanswer <atlanswer@gmail.com>
2022-08-28 16:31:11 +0800

    add .gitignore
    (config) using conda environment
    
    Python package
    
    (config) update pyproject.toml
    
    (config) attach conda-lock.yml
    
    (config) update dependencies
    
    config: add hatch
    
    add HFSS class
    
    relative import
    
    self import
    
    rename submodule sim
    
    save before solve
    
    update dra
    
    context manager
    
    create dra setup
    
    build model
    
    fix material import
    
    docs: add badges
    
    feat: add sphinx doc
    
    feat(docs): autodoc2, intersphinx, viewcode, furo, myst
    
    docs: hfss
    
    docs: readthedocs.yaml
    
    docs: add rtd badge
    
    docs: switch to sphinx-rtd-theme
    
    chore: add logging
    
    feat: add desktop
    
    fix: remove dra
    
    feat(design): dipole antenna
    
    temp(report): s params
    
    chore(deps): update dependencies and add favicon
    
    back on business

---
+++ cpp/include/antcal/antcal.hpp
@@ -0,0 +1,15 @@
+#ifndef ANTCAL_DEF
+
+#define ANTCAL_DEF
+
+#include "version.hpp"
+
+namespace antcal {
+const char VERSION[]{ANTCAL_VERSION};
+
+// ! Only for testing doctest.
+// TODO: remove this.
+int factorial(int number);
+} // namespace antcal
+
+#endif /* ANTCAL_DEF */
