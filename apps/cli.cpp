1537011d735b945974d8f224c057bfa820688bef
Atlanswer <atlanswer@gmail.com>
2022-08-28 17:56:19 +0800

    add initial cli files

---
+++ apps/cli.cpp
@@ -0,0 +1,8 @@
+#include <fmt/core.h>
+
+#include "antcal/antcal.hpp"
+
+int main(int argc, char **argv) {
+    fmt::print("Hello, world!\nantcal version: {}.\n", antcal::VERSION);
+    return 0;
+}
