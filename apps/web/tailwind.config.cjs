d7432079554baa996272dada2d096a4ec6ce7588
Atlanswer <atlanswer@gmail.com>
2023-05-22 10:39:25 +0800

    Add Next.js web app
    add copy design variables script
    
    move qwik project
    
    small update
    
    nextjs template with turborepo

---
+++ apps/web/tailwind.config.cjs
@@ -0,0 +1,7 @@
+// tailwind config is required for editor support
+
+const sharedConfig = require("tailwind-config/tailwind.config.cjs");
+
+module.exports = {
+  presets: [sharedConfig],
+};
--- python/src/antcal/__init__.py
+++ python/src/antcal/__init__.py
@@ -1,4 +1,4 @@
 """AntCal: Antenna Calculator.
 """
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
