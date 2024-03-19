6081c4f5b5771ec750cef8afd7017040161c3f9e
Atlanswer <atlanswer@gmail.com>
2024-03-20 04:09:37 +0800

    eslint config is stupid

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,57 @@
+// @ts-expect-error No type provided
+import { FlatCompat } from "@eslint/eslintrc";
+// @ts-expect-error No type provided
 import eslint from "@eslint/js";
+// @ts-expect-error No type provided
 import prettier from "eslint-config-prettier";
+// @ts-expect-error No type provided
+import solid from "eslint-plugin-solid/configs/typescript.js";
+import globals from "globals";
 import tsEslint from "typescript-eslint";
+
+// const compat = new FlatCompat({
+// baseDirectory: "apps/figure",
+// });
+
+const ts = tsEslint.configs.recommendedTypeChecked;
 
 export default tsEslint.config(
   eslint.configs.recommended,
   ...tsEslint.configs.recommended,
+  {
+    files: ["apps/figure/**/*.{js,ts,tsx}"],
+    plugins: ts[0].plugins,
+    languageOptions: {
+      parser: ts[0].languageOptions.parser,
+      sourceType: ts[0].languageOptions.sourceType,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+      globals: { ...globals["shared-node-browser"] },
+    },
+    rules: {
+      ...ts[1].rules,
+      ...ts[2].rules,
+    },
+    // ...solid,
+    // ...compat.config({ extends: ["turbo"] }),
+  },
+  {
+    files: ["packages/**/*.js"],
+    languageOptions: {
+      globals: { ...globals.nodeBuiltin },
+    },
+  },
+  {
+    ignores: [
+      "eslint.config.js",
+      "**/.output",
+      "**/.turbo",
+      "**/.vinxi",
+      "**/public",
+      "**/venv",
+    ],
+  },
   prettier,
 );
