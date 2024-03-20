1a5ad45a647d8a1d131015798351d0d9528287d2
Atlanswer <atlanswer@gmail.com>
2024-03-20 12:37:34 +0800

    404 route

--- apps/figure/src/routes/[...404].tsx
+++ apps/figure/src/routes/[...404].tsx
@@ -1,19 +1,16 @@
 import { Title } from "@solidjs/meta";
+import { useParams } from "@solidjs/router";
 import { HttpStatusCode } from "@solidjs/start";
 
 export default function NotFound() {
+  const a = useParams<{ 404: string }>();
+
   return (
     <main>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
       <h1>Page Not Found</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <code>{a[404]}</code>
     </main>
   );
 }
