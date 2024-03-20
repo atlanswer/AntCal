499ca58f80574e5d058a41271c7e73a16a23a0a1
Atlanswer <atlanswer@gmail.com>
2024-03-20 12:16:52 +0800

    root layout

--- apps/figure/src/entry-server.tsx
+++ apps/figure/src/entry-server.tsx
@@ -4,15 +4,33 @@ import { createHandler, StartServer } from "@solidjs/start/server";
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
-      <html lang="en">
+      <html lang="en-US" class="h-full">
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+          <meta name="color-scheme" content="light dark" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: dark)"
+            content="#000000"
+          />
+          <link
+            rel="manifest"
+            type="application/manifest+json"
+            href="/app.webmanifest"
+          />
           {assets}
         </head>
-        <body>
-          <div id="app">{children}</div>
+        <body
+          id="app"
+          class="flex h-full min-w-64 flex-col font-sans antialiased motion-reduce:transition-none"
+        >
+          <noscript>
+            AntCal: You need to enable JavaScript to run this app.
+          </noscript>
+          {children}
           {scripts}
         </body>
       </html>
