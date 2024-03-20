302d34cfd71ee5fcf1d1e0a0a5b2cbe8cd31d5f4
Atlanswer <atlanswer@gmail.com>
2024-03-21 03:38:35 +0800

    vercel static preset

--- apps/figure/app.config.ts
+++ apps/figure/app.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from "@solidjs/start/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  ssr: false,
-  experimental: {
-    islands: true,
-  },
   vite: {
     plugins: [tsconfigPaths()],
   },
   server: {
-    preset: "static",
-    prerender: {
-      crawlLinks: true,
-    },
+    preset: "vercel-static",
+    future: { nativeSWR: true },
+    prerender: { crawlLinks: true },
   },
 });
