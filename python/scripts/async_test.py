80dbed37597d2b8e7620a65ac772e2d4625f7d35
atlanswer <i@atlanswer.com>
2024-05-20 10:58:36 +0800

    switch to pipenv

--- python/scripts/async_test.py
+++ python/scripts/async_test.py
@@ -1,37 +1,47 @@
 import asyncio
-from loguru import logger
+import logging
 from random import random
+
 import numpy as np
+from rich.logging import RichHandler
+
+logging.basicConfig(
+    level="NOTSET",
+    format="%(message)s",
+    datefmt="[%m/%d %H:%M:%S]",
+    handlers=[RichHandler(markup=True)],
+)
+log = logging.getLogger("rich")
 
 
 async def solve(workers: asyncio.Queue[int], input: int) -> int:
     worker = await workers.get()
-    logger.info(f"Task start, {input=} assigned to {worker=}.")
+    log.info(f"Task start, {input=} assigned to {worker=}.")
     progress = 0
 
     while progress < 1:
         work = random()
         await asyncio.sleep(work)
         progress += work
-        logger.info(f"{progress=:.2f}")
+        log.info(f"{progress=:.2f}")
 
-    logger.info(f"Task finished, final {progress=:.2f}, {worker=} queued.")
+    log.info(f"Task finished, final {progress=:.2f}, {worker=} queued.")
     await workers.put(worker)
     return input * 2
 
 
 async def orchestrator(n_workers: int, inputs: list[int]):
-    workers = asyncio.Queue()
+    workers: asyncio.Queue[int] = asyncio.Queue()
     for i in range(1, n_workers + 1):
         await workers.put(i)
-    logger.info("Worker queue created.")
+    log.info("Worker queue created.")
 
     async with asyncio.TaskGroup() as tg:
         tasks = [tg.create_task(solve(workers, i)) for i in inputs]
 
     results = np.vstack([task.result() for task in tasks])
 
-    logger.info(f"All tasks finished, {results=}, {results.shape=}")
+    log.info(f"All tasks finished, {results=}, {results.shape=}")
 
 
 def main():
