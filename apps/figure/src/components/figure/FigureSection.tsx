9edb792602a27c5b6bcac560d2998b6ebb8d82f7
Atlanswer <i@atlanswer.com>
2024-06-09 13:31:15 +0800

    move edit icon

--- apps/figure/src/components/figure/FigureSection.tsx
+++ apps/figure/src/components/figure/FigureSection.tsx
@@ -1,6 +1,7 @@
 import { Show, type Accessor, type Component } from "solid-js";
 import { useFigureConfigs, type FigureConfig } from "./context";
 import { produce } from "solid-js/store";
+import { EditIcon } from "../icons/Edit";
 
 export const FigureSection: Component<{
   figureConfig: Accessor<FigureConfig>;
@@ -13,26 +14,18 @@ export const FigureSection: Component<{
       <figure class="flex max-w-full flex-col gap-2">
         <figcaption class="flex place-content-between place-items-center gap-4">
           <div class="flex flex-wrap place-items-center gap-4">
-            <div class="relative">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                class="absolute left-2 top-2 h-6 w-6 fill-neutral-500"
-              >
-                <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
-                <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
-              </svg>
+            <div class="relative last:right-0">
               <input
                 type="text"
                 name="Figure Title"
                 placeholder="Figure Title"
-                class="w-72 max-w-full rounded bg-neutral-100 px-2 py-1 pl-10 text-2xl font-semibold text-black shadow focus-visible:outline-none focus-visible:ring dark:bg-neutral-800 dark:text-white"
+                class="w-72 max-w-full rounded bg-neutral-100 px-2 py-1 pr-10 text-2xl font-semibold text-black shadow focus-visible:outline-none focus-visible:ring dark:bg-neutral-800 dark:text-white"
                 value={props.figureConfig().title}
                 onChange={(event) =>
                   setFigureConfigs(props.idx, "title", event.target.value)
                 }
               />
+              <EditIcon class="absolute right-2 top-2" />
             </div>
             <div
               title="Switch axes scale"
