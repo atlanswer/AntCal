c7ef17df61967ef7d04a1398f391f4a997a90429
Atlanswer <atlanswer@gmail.com>
2024-11-17 20:53:57 +0800

    change header text size

--- apps/figure/src/components/Header.tsx
+++ apps/figure/src/components/Header.tsx
@@ -25,13 +25,13 @@ const Navigator = () => {
   ];
 
   return (
-    <nav class="flex flex-1 text-xl text-black sm:place-content-center dark:text-white">
+    <nav class="flex flex-1 sm:place-content-center">
       <span class="inline-block [&>:not(:first-child)]:ml-6">
         <For each={routes}>
           {(route) => (
             <A
               href={route.route}
-              class="box-border inline-block border-b-2 border-transparent px-1 pb-[calc(1rem-2px)] pt-4 focus-visible:outline-none focus-visible:ring [&.active]:border-sky-500 [&.inactive]:hover:border-neutral-300 dark:[&.inactive]:hover:border-neutral-700"
+              class="saira-header box-border inline-block border-b-2 border-transparent px-1 pb-[calc(1rem-2px)] pt-4 text-2xl tracking-wide focus-visible:outline-none focus-visible:ring [&.active]:border-sky-500 [&.inactive]:hover:border-neutral-300 dark:[&.inactive]:hover:border-neutral-700"
               end
             >
               {route.name}
@@ -44,7 +44,7 @@ const Navigator = () => {
 };
 
 export const Header = () => (
-  <header class="saira-header sticky top-0 z-10 place-content-center bg-white px-8 text-black shadow dark:border-neutral-900 dark:bg-black dark:text-white">
+  <header class="sticky top-0 z-10 place-content-center bg-white px-8 text-black shadow dark:border-neutral-900 dark:bg-black dark:text-white">
     <div class="mx-auto flex max-w-screen-xl place-content-between place-items-center gap-8">
       <span class="hidden w-8 sm:inline" />
       <Navigator />
