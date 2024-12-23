d258b2a9217093676e31570f94335b7cc5592d7f
atlanswer <i@atlanswer.com>
2024-12-23 14:47:23 +0800

    update dependencies

--- packages/tailwind-config/postcss.config.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Config } from "postcss-load-config";
-export type { Config };
-
-declare const sharedConfig: Config;
-export default sharedConfig;
--- packages/tailwind-config/tailwind.config.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Config } from "tailwindcss";
-export type { Config };
-
-declare const sharedConfig: Partial<Config>;
-export default sharedConfig;
