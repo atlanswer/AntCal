707c7977fca4147ac6bd7d0c62c72dd1c47cd52a
Atlanswer <atlanswer@gmail.com>
2025-05-22 22:18:03 +0800

    migrate astro

--- apps/astro/astro.config.ts
+++ apps/astro/astro.config.ts
@@ -1,7 +1,58 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        name: "Geist Sans",
+        provider: "local",
+        cssVariable: "--font-geist-sans",
+        variants: [
+          {
+            weight: "100 900",
+            src: [
+              "../../node_modules/geist/dist/fonts/geist-sans/Geist-Variable.woff2",
+            ],
+          },
+        ],
+        fallbacks: [
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+      {
+        name: "Geist",
+        provider: "local",
+        cssVariable: "--font-geist-mono",
+        variants: [
+          {
+            weight: "100 900",
+            src: [
+              "../../node_modules/geist/dist/fonts/geist-mono/GeistMono-Variable.woff2",
+            ],
+          },
+        ],
+        fallbacks: [
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          "Liberation Mono",
+          "Courier New",
+          "monospace",
+        ],
+      },
+    ],
+  },
 });
--- apps/astro/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-import typography from "@tailwindcss/typography";
-
-/** @type {import("tailwindcss").Config} */
-export default {
-  content: ["./src/**/*.{astro,tsx,ts,md,mdx,css}"],
-  darkMode: "class",
-  plugins: [typography],
-};
