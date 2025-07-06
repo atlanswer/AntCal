ed024de5669290ce84c0bfd2c68eac9866f45746
Atlanswer <atlanswer@gmail.com>
2025-07-07 00:39:33 +0800

    import tailwind stylesheet into the downloaded svg

--- apps/figure/src/components/field/FieldPlot.tsx
+++ apps/figure/src/components/field/FieldPlot.tsx
@@ -262,8 +262,24 @@ function SVGDownload(props: { target: SVGSVGElement }) {
     <button
       type="button"
       class="rounded bg-sky-500 px-4 py-2 font-semibold text-white hover:bg-sky-700"
-      onClick={() => {
-        const svgData = new XMLSerializer().serializeToString(props.target);
+      onClick={async () => {
+        const svg = props.target.cloneNode(true);
+        const defs = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "defs",
+        );
+        svg.insertBefore(defs, svg.firstChild);
+        const style = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "style",
+        );
+        defs.appendChild(style);
+
+        const globalStyleText = (await import("styles/global.css?inline"))
+          .default;
+        style.textContent = globalStyleText;
+
+        const svgData = new XMLSerializer().serializeToString(svg);
         const svgBlob = new Blob([svgData], { type: "image/svg+xml" });
         const svgUrl = URL.createObjectURL(svgBlob);
 
