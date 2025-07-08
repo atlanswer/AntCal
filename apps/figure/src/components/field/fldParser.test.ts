8e7066516c629208f36aaf4188c56c29c265cae0
Atlanswer <atlanswer@gmail.com>
2025-07-09 04:34:43 +0800

    update test

---
+++ apps/figure/src/components/field/fldParser.test.ts
@@ -0,0 +1,11 @@
+import { parseFld } from "components/field/fldParser";
+import { expect, test, describe } from "bun:test";
+
+test("2 + 2 = 4", () => {
+  expect(2 + 2).toBe(4);
+});
+
+describe(".fld Parser", async () => {
+
+  test("Vector Grid", () => {});
+});
--- apps/figure/src/components/field/fldParser.ts
+++ apps/figure/src/components/field/fldParser.ts
@@ -1,3 +1,13 @@
+type ScalarArray = { x: number; y: number; z: number; value: number }[];
+type VectorArray = {
+  x: number;
+  y: number;
+  z: number;
+  u: number;
+  v: number;
+  w: number;
+}[];
+
 function* genLines(text: string) {
   const lines = text.split("\n");
   for (const line of lines) {
@@ -5,12 +15,29 @@ function* genLines(text: string) {
   }
 }
 
-function parseGridInfo(line: string) {}
+function parseGridDimension(line: string) {}
+
+function parseDataDescription(line: string) {}
+
+function parseElementNumber(line: string): number {
+  const numPart = line.trim().split(" ").pop();
+  return parseInt(numPart ?? "NaN");
+}
 
 export function parseFld(text: string) {
   for (const line of genLines(text)) {
     if (line.startsWith("Grid Output")) {
-      parseGridInfo(line);
+      parseGridDimension(line);
+      continue;
     }
+    if (line.includes(`"`)) {
+      parseDataDescription(line);
+    }
+    if (line.startsWith("NumElems")) {
+      parseElementNumber(line);
+    }
+
+    console.error("Unrecognized lines:");
+    console.log(line);
   }
 }
