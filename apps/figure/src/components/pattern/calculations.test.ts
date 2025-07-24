ae4e3471e90977b51383ebc18364b552d6798d0e
Atlanswer <atlanswer@gmail.com>
2025-07-24 14:04:36 +0800

    correct rotation

--- apps/figure/src/components/pattern/calculations.test.ts
+++ apps/figure/src/components/pattern/calculations.test.ts
@@ -2,67 +2,87 @@ import { test, expect, describe } from "bun:test";
 import { rotateCoordinate } from "components/pattern/calculations";
 
 describe("Coordinate Rotation", () => {
-  test("No Rotation", () => {
-    const { theta, phi } = rotateCoordinate(
-      { theta: Math.PI / 2, phi: Math.PI / 3 },
-      0,
-      0,
-    );
-    expect(theta).toBeCloseTo(Math.PI / 2);
-    expect(phi).toBeCloseTo(Math.PI / 3);
+  describe("No Rotation", () => {
+    test("Test 1", () => {
+      const { theta, phi } = rotateCoordinate(
+        { theta: Math.PI / 2, phi: 0 },
+        0,
+        0,
+      );
+      expect(theta).toBeCloseTo(Math.PI / 2);
+      expect(phi).toBeCloseTo(0);
+    });
+    test("Test 2", () => {
+      const { theta, phi } = rotateCoordinate(
+        { theta: Math.PI / 4, phi: Math.PI / 3 },
+        0,
+        0,
+      );
+      expect(theta).toBeCloseTo(Math.PI / 4);
+      expect(phi).toBeCloseTo(Math.PI / 3);
+    });
   });
-  test("Original North Pole", () => {
-    const { theta, phi } = rotateCoordinate(
-      { theta: 0, phi: 0 },
-      Math.PI / 3,
-      Math.PI / 4,
-    );
-    expect(theta).toBeCloseTo(Math.PI / 3);
-    expect(phi).toBeCloseTo(-Math.PI);
+
+  describe("Pure ϕ-Rotation", () => {
+    test("Test 1", () => {
+      const { theta, phi } = rotateCoordinate(
+        { theta: Math.PI / 2, phi: 0 },
+        0,
+        Math.PI / 2,
+      );
+      expect(theta).toBeCloseTo(Math.PI / 2);
+      expect(phi).toBeCloseTo(-Math.PI / 2);
+    });
+    test("Test 2", () => {
+      const { theta, phi } = rotateCoordinate(
+        { theta: Math.PI / 2, phi: Math.PI / 2 },
+        0,
+        Math.PI / 2,
+      );
+      expect(theta).toBeCloseTo(Math.PI / 2);
+      expect(phi).toBeCloseTo(0);
+    });
   });
-  test("New North Pole", () => {
-    const { theta, phi } = rotateCoordinate(
-      { theta: Math.PI / 3, phi: Math.PI / 4 },
-      Math.PI / 3,
-      Math.PI / 4,
-    );
-    expect(theta).toBeCloseTo(0);
-    expect(phi).toBeCloseTo(0);
+
+  describe("Pure θ-Rotation", () => {
+    test("Original North Pole", () => {
+      const { theta, phi } = rotateCoordinate(
+        { theta: 0, phi: 0 },
+        Math.PI / 2,
+        0,
+      );
+      expect(theta).toBeCloseTo(Math.PI / 2);
+      expect(phi).toBeCloseTo(Math.PI);
+    });
+    test("Test 2", () => {
+      const { theta, phi } = rotateCoordinate(
+        { theta: Math.PI / 2, phi: 0 },
+        Math.PI / 2,
+        0,
+      );
+      expect(theta).toBeCloseTo(0);
+      expect(phi).toBeCloseTo(0);
+    });
   });
-  test("Pure Longitude Rotation", () => {
-    const { theta, phi } = rotateCoordinate(
-      { theta: Math.PI / 2, phi: 0 },
-      0,
-      Math.PI / 2,
-    );
-    expect(theta).toBeCloseTo(Math.PI / 2);
-    expect(phi).toBeCloseTo(-Math.PI / 2);
-  });
-  test("Original South Pole", () => {
-    const { theta, phi } = rotateCoordinate(
-      { theta: Math.PI, phi: 0 },
-      Math.PI / 2,
-      0,
-    );
-    expect(theta).toBeCloseTo(Math.PI / 2);
-    expect(phi).toBeCloseTo(0);
-  });
-  test("New South Pole", () => {
-    const { theta, phi } = rotateCoordinate(
-      { theta: (Math.PI / 3) * 2, phi: (Math.PI / 4) * 5 },
-      Math.PI / 3,
-      Math.PI / 4,
-    );
-    expect(theta).toBeCloseTo(Math.PI);
-    expect(phi).toBeCloseTo((Math.PI / 2) * 3);
-  });
-  test("Equator Point After 90° Tilt", () => {
-    const { theta, phi } = rotateCoordinate(
-      { theta: Math.PI / 2, phi: Math.PI / 2 },
-      Math.PI / 2,
-      0,
-    );
-    expect(theta).toBeCloseTo(Math.PI / 2);
-    expect(phi).toBeCloseTo(Math.PI / 2);
+
+  describe("Combined Rotation", () => {
+    test("Original North Pole", () => {
+      const { theta, phi } = rotateCoordinate(
+        { theta: 0, phi: 0 },
+        Math.PI / 2,
+        Math.PI / 2,
+      );
+      expect(theta).toBeCloseTo(Math.PI / 2);
+      expect(phi).toBeCloseTo(-Math.PI);
+    });
+    test("New North Pole", () => {
+      const { theta, phi } = rotateCoordinate(
+        { theta: Math.PI / 2, phi: Math.PI / 2 },
+        Math.PI / 2,
+        Math.PI / 2,
+      );
+      expect(theta).toBeCloseTo(0);
+      expect(phi).toBeCloseTo(0);
+    });
   });
 });
--- apps/figure/src/components/pattern/calculations.ts
+++ apps/figure/src/components/pattern/calculations.ts
@@ -5,16 +5,22 @@ export function rotateCoordinate(
   theta0: number,
   phi0: number,
 ): Coordinate {
+  const x =
+    Math.cos(theta0) * Math.sin(v.theta) * Math.cos(v.phi - phi0) -
+    Math.sin(theta0) * Math.cos(v.theta);
+  const y = Math.sin(v.theta) * Math.sin(v.phi - phi0);
+  let z =
+    Math.sin(theta0) * Math.sin(v.theta) * Math.cos(v.phi - phi0) +
+    Math.cos(theta0) * Math.cos(v.theta);
+
+  z = z > 1 ? 1 : z;
+  z = z < -1 ? -1 : z;
+
+  const eps = 1e-10;
+
   return {
-    theta: Math.acos(
-      Math.sin(theta0) * Math.sin(v.theta) * Math.cos(v.phi - phi0) +
-        Math.cos(theta0) * Math.cos(v.theta),
-    ),
-    phi: Math.atan2(
-      Math.sin(v.theta) * Math.sin(v.phi - phi0),
-      Math.cos(theta0) * Math.sin(v.theta) * Math.cos(v.phi - phi0) -
-        Math.sin(theta0) * Math.cos(v.theta),
-    ),
+    theta: Math.acos(z),
+    phi: Math.abs(x) < eps && Math.abs(y) < eps ? 0 : Math.atan2(y, x),
   };
 }
 
@@ -29,6 +35,6 @@ export function verticalEDipole(
   );
 }
 
-export function verticalMDipole(theta: number, _: any = undefined): number {
+export function verticalMDipole(theta: number, _: any): number {
   return 1;
 }
